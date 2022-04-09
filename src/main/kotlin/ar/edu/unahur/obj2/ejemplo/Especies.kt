fa21f8900082408658cddee9fbc592077f2cdddf
Brenda_Gauto <89878066+BrendaG86@users.noreply.github.com>
2022-04-09 17:59:57 -0300

    Se crea clase Quinoa , soja transgénica, peperina y se modifica función daSemillas en la interface Planta

--- src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Especies.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Especies.kt
@@ -1,14 +1,11 @@
 package ar.edu.unahur.obj2.ejemplo
 
-class Menta(override val anioObtencion: Int, override val altura: Double) : Planta {
+open class Menta(override val anioObtencion: Int, override val altura: Double) : Planta {
     override fun espacio(): Double{
         return this.altura + 1
     }
     override fun condicionAdicional(): Boolean {
         return this.altura >0.4
-    }
-    override fun daSemillas(): Boolean {
-        return this.esFuerte() || this.condicionAdicional()
     }
     override fun esFuerte(): Boolean {
         return this.horasDeSolToleradas() > 9
@@ -16,15 +13,12 @@ class Menta(override val anioObtencion: Int, override val altura: Double) : Plan
 
 }
 
-class Soja(override val anioObtencion: Int, override val altura: Double) : Planta{
+open class Soja(override val anioObtencion: Int, override val altura: Double) : Planta{
     override fun espacio(): Double {
         return this.altura/2
     }
     override fun condicionAdicional(): Boolean {
         return this.anioObtencion > 2007 && this.altura in 0.75..0.9
-    }
-    override fun daSemillas(): Boolean {
-        return this.esFuerte() || this.condicionAdicional()
     }
     override fun horasDeSolToleradas(): Int {
         val tolerancia: Int = if (this.altura < 0.5){
@@ -41,3 +35,37 @@ class Soja(override val anioObtencion: Int, override val altura: Double) : Plant
     }
 
 }
+
+class Quinoa(override val anioObtencion: Int, override val altura: Double, val espacio: Double) : Planta{
+    override fun horasDeSolToleradas(): Int {
+        val tolerancia: Int = if (this.altura < 0.3){
+            10
+        }else{
+            super.horasDeSolToleradas()
+        }
+        return tolerancia
+    }
+    override fun espacio() = espacio
+
+    override fun condicionAdicional(): Boolean {
+        return this.anioObtencion in 2001..2008
+    }
+    override fun esFuerte(): Boolean {
+        TODO("Not yet implemented")
+    }
+}
+
+//Variedades
+
+class SojaTrangenica(anioObtencion: Int, altura: Double): Soja(anioObtencion, altura){
+
+    override fun daSemillas() = false
+
+}
+
+class Peperina(anioObtencion: Int, altura: Double) : Menta(anioObtencion, altura){
+
+    override fun espacio() = super.espacio() * 2
+
+}
+
--- src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/ejemplo/Planta.kt
@@ -5,10 +5,11 @@ interface Planta {
     val altura: Double
 
     fun horasDeSolToleradas() = 7
-    fun daSemillas(): Boolean
     fun esFuerte(): Boolean
     fun espacio(): Double
     fun condicionAdicional(): Boolean
-
+    fun daSemillas(): Boolean {
+        return this.esFuerte() || this.condicionAdicional()
+    }
 }
 
