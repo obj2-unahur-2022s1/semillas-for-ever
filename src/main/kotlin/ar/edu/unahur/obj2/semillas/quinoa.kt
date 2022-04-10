adf1b16bb1597f6e5be44347565a80151d9ed371
LautaroRiofrio <riofriolautaro@gmail.com>
2022-04-10 19:44:21 -0300

    Creacion clase Quinoa y modificacion de clase planta

---
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/quinoa.kt
@@ -0,0 +1,32 @@
+package ar.edu.unahur.obj2.semillas
+
+class Quinoa(
+    private val fechaDeObtencion: Int,
+    private val altura: Double,
+    private val espacio: Double
+) : Planta(
+    fechaDeObtencion, altura
+){
+    override fun horasDeSolTolera(): Int{
+        var horasQueTolera: Int = 7
+        if (espacio < .3){
+            horasQueTolera = 10
+        }
+        return horasQueTolera
+    }
+
+    override fun daNuevasSemillas(): Boolean {
+        var daSemillas = false
+        if (espacio == .2 && fechaDeObtencion == 2010  || espacio == .9 && fechaDeObtencion == 2006){
+            daSemillas = true
+        }
+        return daSemillas
+    }
+
+    override fun espacioQueOcupa(): Double {
+        return espacio
+    }
+
+
+
+}
