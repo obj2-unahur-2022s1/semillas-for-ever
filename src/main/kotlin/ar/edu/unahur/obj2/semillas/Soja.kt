43aa2c4fac0cc0d443dabd4dc8587b09e3b3b4ea
Tomas <tomasbenjamin117@gmail.com>
2022-04-10 19:42:20 -0300

    creacion de clase soja

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Soja.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Soja.kt
@@ -1,5 +1,23 @@
 package ar.edu.unahur.obj2.semillas
 
-class Soja {
+class Soja(
+    private val fechaDeObtencion: Int,
+    private val altura: Double
+) : Planta(
+    fechaDeObtencion, altura
+) {
+    override fun horasDeSolTolera(): Int {
+        return if(altura < .5) {
+            6
+        } else if (altura in .5..1.0) {
+            8
+        } else {
+            12
+        }
+    }
+
+    override fun daNuevasSemillas(): Boolean {
+        return fechaDeObtencion > 2007 && altura in .75..0.9
+    }
     
 }
