3df42d9b0a41b9893c38f678b536131427659f7e
Tomas <tomasbenjamin117@gmail.com>
2022-04-10 19:31:19 -0300

    creacion de clase menta y clase planta

---
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Planta.kt
@@ -0,0 +1,23 @@
+package ar.edu.unahur.obj2.semillas
+
+open class Planta(
+    fechaDeObtencion: Int,
+    altura: Double
+) {
+    fun horasDeSolTolera(): Int {
+        return 7
+    }
+
+    fun esFuerte(): Boolean {
+        return horasDeSolTolera() > 9
+    }
+
+    open fun daNuevasSemillas(): Boolean {
+        return true
+    }
+
+    open fun espacioQueOcupa(): Double {
+        return 8.0
+    }
+
+}
---
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Soja.kt
@@ -0,0 +1,5 @@
+package ar.edu.unahur.obj2.semillas
+
+class Soja {
+    
+}
