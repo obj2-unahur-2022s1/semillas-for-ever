350198e50875e10b2517128c02b8d2f9c66b6292
Tomas <tomasbenjamin117@gmail.com>
2022-04-10 23:13:02 -0300

    total de plantas

--- src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillas/Parcela.kt
@@ -27,9 +27,7 @@ class Parcela(
         plantas.add(planta)
     }
 
-    fun totalDePlantas(): Int {
-        return plantas.size
-    }
+    fun totalDePlantas(): Int = plantas.size
 
     fun plantas(): List<Planta> = plantas
 }
